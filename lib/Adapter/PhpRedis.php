3b295e0c0b0040c5d99bbcbef1b3f9c4832d9e38
Wes Roberts <wes@sdynamics.com>
2011-11-30 18:58:33 -0500

    Started PhpRedis adapter

---
+++ lib/Adapter/PhpRedis.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ActiveRedis;
+
+class Adapter\PhpRedis extends Adapter
+{
+	protected $db;
+	protected $config;
+	protected $initialized;
+	
+	function __construct($config = null, $redisObject = null)
+	{
+		// Allow user to provide a Redis object
+		if ($redisObject) {
+			$this->db = $redisObject;
+			$this->initialized = true;
+		} 
+		
+		// But make a new Redis by default
+		else {
+			$this->db = new Redis;
+		}
+		
+		// Store the config for later
+		if ($config) {
+			if (!is_array($config)) {
+				$config['connect'] = $config;
+			}
+			$this->config = $config;
+		}
+	}
+	
+	function initialize()
+	{
+		if (!$this->initialized) {
+			$init = isset($this->config['initialize']) ? (array) $this->config['initialize'] : array('connect', 'open', 'pconnect', 'popen');
+			foreach ($init as $fn) {
+				if (is_string($fn)) {
+					if (isset($this->config[$fn])) {
+						call_user_func_array(array($this->db, $fn), array_force($this->config[$fn]));
+					}
+				} elseif (is_callable($fn)) {
+					$fn($this);
+				}
+			}
+		}
+	}
+	
+	function __call($fn, $args)
+	{
+		$this->initialize();
+		return parent::__call($fn, $args);
+	}
+}
+
+?>
